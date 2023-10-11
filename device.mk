3b6cd64a50d8af25d47a92ac454135ce93445682
ziadtamer <ziadtamer150@gmail.com>
2023-10-11 23:51:01 +0300

    Initial minimised rootdir

--- device.mk
+++ device.mk
@@ -10,6 +10,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6893 \
+    fstab.mt6893.ramdisk \
+
+PRODUCT_PACKAGES += \
+    init.cgroup.rc \
+    init.modem.rc \
+    init.mt6893.rc \
+    init.mt6893.usb.rc \
+    init.project.rc \
+    init.recovery.mt6893.rc \
+    init.sensor_2_0.rc \
+    init.stnfc.rc \
+    ueventd.mt6893.rc
+
+PRODUCT_PACKAGES_ENG += \
+    init.debug.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
