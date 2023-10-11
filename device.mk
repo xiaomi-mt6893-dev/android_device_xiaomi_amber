bb140644819e9b869f50302595fcb07884ee7449
ziadtamer <ziadtamer150@gmail.com>
2023-10-12 00:14:06 +0300

    Build default health HIDL

--- device.mk
+++ device.mk
@@ -45,6 +45,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
