57b211f15dd1f4d03d06a3f4fdc3ec8088924e37
ziadtamer <ziadtamer150@gmail.com>
2023-10-11 23:55:36 +0300

    Configure dynamic partitions

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
