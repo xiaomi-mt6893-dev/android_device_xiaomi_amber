5c9f6c853e8390cedf77800535092dc8ba09a3e1
ziadtamer <ziadtamer150@gmail.com>
2023-10-13 01:34:03 +0300

    Configure Neural Networks stack

--- device.mk
+++ device.mk
@@ -240,6 +240,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor \
+    libtextclassifier_hash.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
