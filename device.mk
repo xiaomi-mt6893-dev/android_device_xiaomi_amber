c4d0156bc750ee69f88ba865916b44d25584dc08
ziad <ziadtamer150@gmail.com>
2023-11-23 13:49:22 +0200

    amber: Build android.hardware.media.c2@1.0.so
    CANNOT LINK EXECUTABLE /vendor/bin/hw/android.hardware.media.c2@1.2-mediatek: library android.hardware.media.c2@1.0.so not found: needed by main executable

--- device.mk
+++ device.mk
@@ -197,6 +197,9 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.0.vendor \
+    android.hardware.media.c2@1.1.vendor \
+    android.hardware.media.c2@1.2.vendor \
     libcodec2_hidl@1.2.vendor \
     libavservices_minijail_vendor \
     libstagefright_softomx_plugin.vendor \
