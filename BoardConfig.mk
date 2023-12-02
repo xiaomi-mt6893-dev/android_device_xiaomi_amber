5aeab0f73d9abdd86fe09352e8f2b776fe105fdc
rio004 <riyon336@gmail.com>
2023-12-02 17:15:21 +0000

    amber: Force beanpod encryption
     amber has two different encryptions, the kernel cmdline seems
     to dictate which one to use. Since I already removed MITEE
     forcing to beanpod encryption is the easiest way to fix this.
    
    Change-Id: I2975278cab2da615925bd9278803641493f159e9
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,8 @@ BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
 
 BOARD_KERNEL_CMDLINE := \
     androidboot.init_fatal_reboot_target=recovery \
-    bootopt=64S3,32N2,64N2
+    bootopt=64S3,32N2,64N2 \
+    androidboot.tee_type=2
 
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
