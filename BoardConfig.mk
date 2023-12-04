7b5e46a99c1db8296f36460ee36b959d78dec65c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-04 17:35:47 +0000

    amber: Declare BOARD_VENDOR

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 # Platform
 BOARD_HAS_MTK_HARDWARE := true
 TARGET_BOARD_PLATFORM := mt6893
+BOARD_VENDOR := xiaomi
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
