36559ab5eea4647bcf11f34a10576bf815598ee1
ziad <ziadtamer150@gmail.com>
2023-12-10 03:21:30 +0200

    amber: Build missing thermal service

--- device.mk
+++ device.mk
@@ -352,6 +352,11 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@2.0.vendor
+
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
