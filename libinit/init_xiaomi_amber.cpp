f7bb50613e45f26c5574e14b631c1d20704edc99
rio004 <riyon336@gmail.com>
2024-01-04 16:35:23 +0000

    amber: Import libinit from sm8250-common

---
+++ libinit/init_xiaomi_amber.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_variant.h>
+#include <libinit_utils.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t amber_info = {
+    .hwc_value = "",
+    .sku_value = "amber",
+
+    .brand = "Xiaomi",
+    .device = "amber",
+    .marketname = "Xiaomi 11T",
+    .model = "21081111RG",
+    .build_fingerprint = "Xiaomi/amber_global/amber:12/SP1A.210812.016/V14.0.5.0.TKWMIXM:user/release-keys",
+};
+
+static const std::vector<variant_info_t> variants = {
+    amber_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+}
