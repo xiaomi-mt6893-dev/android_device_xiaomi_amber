5fa5ce89a01168793f4eb850d5fc42b6de25d571
luk1337 <priv.luk@gmail.com>
2024-01-09 19:28:04 +0200

    amber: sepolicy: Label wakeup nodes for i in $(realpath /sys/class/wakeup/*); do if ! echo "$(ls -dZ $i)" | grep -q sysfs_wakeup; then echo genfscon sysfs $(echo $i | sed -e 's|/sys||g' -e 's|wakeup/wakeup.|wakeup|g' -e 's|wakeup[0-9]*|wakeup|g') u:object_r:sysfs_wakeup:s0; fi; done | sort -u
    Change-Id: Id70f4e725f8925b31c0007fae5eb98756ddcd410

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,7 @@ VENDOR_SECURITY_PATCH := 2023-07-01
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
