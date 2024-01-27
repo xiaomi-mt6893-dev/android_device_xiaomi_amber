ece4563fefc7a3453f3090d101ab1040fcb50248
bengris32 <bengris32@protonmail.ch>
2024-01-27 05:45:25 +0200

    amber: Switch Boot HAL to AIDL
    Change-Id: Ic921cb8e3f5a70665f09c9506e5ab8311daaa904
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -23,9 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 # A/B
 PRODUCT_PACKAGES += \
