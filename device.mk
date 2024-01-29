58866a276478a48633b0e180295deae0f51b889c
bengris32 <bengris32@protonmail.ch>
2024-01-29 05:52:18 +0200

    amber: Switch to common QTI vibrator service
    Change-Id: Ib58c8f5cd33ffbf0cf8c18ceb14c81cb21b1b2f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -359,11 +359,13 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.1.vendor
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-   vendor.qti.hardware.vibrator.service.amber
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
