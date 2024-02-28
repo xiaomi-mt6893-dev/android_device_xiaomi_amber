ec5562d58865b8cafbbac34fd75f268b54f972c0
bengris32 <bengris32@protonmail.ch>
2024-02-28 22:38:02 +0000

    amber: parts: Import from sm8250-common
    * From LineageOS/android_device_xiaomi_sm8250-common @ 62e8e0551849a1c2d4f1d35a328d5eb70330bf53
    * Drop all features apart from thermal.
    
    Change-Id: I39173f743d0d29dbcc3d88390830ed33570fed56
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/com.android.nfc_extras.xml
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
