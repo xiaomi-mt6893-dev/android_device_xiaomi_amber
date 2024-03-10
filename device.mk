ef7e628c1fcc28b605ebedf9c1b48a35af652c90
luk1337 <priv.luk@gmail.com>
2024-03-10 20:40:09 +0200

    amber: Build `libprotobuf-cpp-lite-3.9.1-vendorcompat`
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4.vendor:64
+    android.hardware.drm@1.4.vendor \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 PRODUCT_PACKAGES += \
     libdrmclearkeyplugin:64 \
